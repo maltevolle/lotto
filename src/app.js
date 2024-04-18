30776a2e0f4dc458291d84f9db08c2f86785c87a
harun <harun.cebeci@ruhr-uni-bochum.de>
2024-04-18 14:12:03 +0200

    Buttons, Textfield added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
 const app = Vue.createApp({
-    template: '<h1>Hallo, Welt!</h1>'
+    data() {
+        return {
+            buttonNumbers: []
+        };
+    },
+    methods: {
+        handleButtonClick(number) {
+            console.log("Button clicked:", number);
+            // Hier kannst du die Logik für den Klick-Handler implementieren
+        }
+    },
+    mounted() {
+        // Fülle das Array mit den Zahlen von 1 bis 49
+        for (let i = 1; i <= 49; i++) {
+            this.buttonNumbers.push(i);
+        }
+    }
 });
 
 app.mount('#app');
