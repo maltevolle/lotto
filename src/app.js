f57f4f22487c01611c8e7e1f6da274661410fc71
harun <harun.cebeci@ruhr-uni-bochum.de>
2024-04-23 12:25:54 +0200

    Change colors when Buttons clicked, Returning clicked Buttons when clicking on Absenden Button

--- src/app.js
+++ src/app.js
@@ -1,13 +1,42 @@
 const app = Vue.createApp({
     data() {
         return {
-            buttonNumbers: []
+            buttonNumbers: [],
+            selectedNumbers: [] // Array, um die ausgewählten Zahlen zu speichern
         };
     },
     methods: {
         handleButtonClick(number) {
-            console.log("Button clicked:", number);
-            // Hier kannst du die Logik für den Klick-Handler implementieren
+            // Überprüfen, ob die maximale Anzahl von 6 Buttons erreicht wurde
+            if (this.selectedNumbers.length >= 6) {
+                // Überprüfen, ob die ausgewählte Zahl bereits ausgewählt wurde
+                if (this.selectedNumbers.includes(number)) {
+                    // Wenn die Zahl bereits ausgewählt wurde, entferne sie aus dem Array
+                    const index = this.selectedNumbers.indexOf(number);
+                    this.selectedNumbers.splice(index, 1);
+                } else {
+                    // Ansonsten informiere den Benutzer, dass er nicht mehr als 6 Buttons auswählen kann
+                    console.log("Sie können nicht mehr als 6 Buttons auswählen.");
+                }
+            } else {
+                // Füge die ausgewählte Zahl zum Array hinzu
+                if (this.selectedNumbers.includes(number)) {
+                    // Wenn die Zahl bereits ausgewählt wurde, entferne sie aus dem Array
+                    const index = this.selectedNumbers.indexOf(number);
+                    this.selectedNumbers.splice(index, 1);
+                } else {
+                    this.selectedNumbers.push(number);
+                }
+            }
+        },
+        submitForm() {
+            if(this.selectedNumbers.length < 6)
+            {
+                console.log("Zu wenig Zahlen ausgewählt!"); // Ausgabe der ausgewählten Zahlen
+            } else
+            {
+                console.log("Ausgewählte Zahlen:", this.selectedNumbers); // Ausgabe der ausgewählten Zahlen
+            }
         }
     },
     mounted() {
