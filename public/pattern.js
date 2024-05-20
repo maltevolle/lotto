c08c0db135cf796404d67dace496bdfadf7483a9
harun <harun.cebeci@ruhr-uni-bochum.de>
2024-05-20 12:21:24 +0200

    Changed isNextToEachOther Pattern to atleastThreeAreNextToEachother Added isPrime Pattern

--- public/pattern.js
+++ public/pattern.js
@@ -17,7 +17,8 @@ const matrix = createMatrix(7,7);
 export function checkPattern(selectedNumbers) {
     isEven(selectedNumbers);
     isDiagoanal(selectedNumbers);
-    isNextToEachother(selectedNumbers)
+    atleastThreeAreNextToEachother(selectedNumbers)
+    isPrime(selectedNumbers);
     return dict;
 }
 
@@ -52,18 +53,41 @@ function isDiagoanal(selectedNumbers)
     return dict["isDiagonal"] = true;
 }
 
-function isNextToEachother(selectedNumbers)
+function atleastThreeAreNextToEachother(selectedNumbers)
 {
-    dict["isNextToEachother"] = false;
+    dict["atleastThreeAreNextToEachother"] = false;
     let copySelectedNumbers = selectedNumbers.slice();
     copySelectedNumbers.sort((a, b) => a - b);
-    for (let i = 1; i < copySelectedNumbers.length - 1; i++) {
-        if ((copySelectedNumbers[i+1] - copySelectedNumbers[i]) !== 1)
-        {
-            return dict["isNextToEachother"] = false;
+    for (let i = 0; i <= copySelectedNumbers.length - 3; i++) {
+        if (copySelectedNumbers[i] + 1 === copySelectedNumbers[i + 1] && copySelectedNumbers[i + 1] + 1 === copySelectedNumbers[i + 2]) {
+            return dict["atleastThreeAreNextToEachother"] = true;
         }
     }
-    return dict["isNextToEachother"] = true;
+
+    return dict["atleastThreeAreNextToEachother"] = false;
 }
 
+function isPrime(selectedNumbers)
+{
+    dict["isPrime"] = [false, 0];
+    function isPrime(num) {
+        if (num <= 1) return false;
+        if (num <= 3) return true;
+        if (num % 2 === 0 || num % 3 === 0) return false;
+        for (let i = 5; i * i <= num; i += 6) {
+            if (num % i === 0 || num % (i + 2) === 0) return false;
+        }
+        return true;
+    }
+
+    let primeCount = 0;
+
+    for (let i = 0; i < selectedNumbers.length; i++) {
+        if (isPrime(selectedNumbers[i])) {
+            primeCount++;
+        }
+    }
+
+    return dict["isPrime"] = [primeCount > 0, primeCount];
+}
 
