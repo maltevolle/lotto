39a9d10c32a3fbd4151c1a402782d9388dd86480
maltevolle <130567473+maltevolle@users.noreply.github.com>
2024-06-11 11:58:03 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from db import Base, Entry, engine, Database
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from pathlib import Path
 
 app = FastAPI()
 database = Database()
@@ -11,13 +14,22 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Erlauben Sie OPTIONS-Anfragen
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
 )
 
-@app.get('/')
+# Serve static files
+app.mount("/static", StaticFiles(directory=Path(__file__).parent.parent / "frontend" / "public"), name="static")
 
-@app.post('/')
+@app.get("/")
+async def read_root():
+    return FileResponse(Path(__file__).parent.parent / "frontend" / "public" / "index.html")
+
+@app.get("/favicon.ico")
+async def favicon():
+    return FileResponse(Path(__file__).parent.parent / "frontend" / "public" / "favicon.ico")
+
+@app.post('/api/submitForm')
 async def receive_selected_numbers(submittedinfo: dict):
     selected_numbers = submittedinfo.get('selectedNumbers')
     input_text = submittedinfo.get('inputText')
@@ -27,3 +39,10 @@ async def receive_selected_numbers(submittedinfo: dict):
     print(message)
     database.save_submittedinfo(selected_numbers, input_text, pattern)
     return {"message": message}
+
+if __name__ == "__main__":
+    try:
+        import uvicorn
+        uvicorn.run(app, host="0.0.0.0", port=8000)
+    except KeyboardInterrupt:
+        print("Server interrupted and shut down")
