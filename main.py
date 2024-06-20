d8e355783324a27c751977ca46ec31b29b2eca48
maltevolle <130567473+maltevolle@users.noreply.github.com>
2024-06-20 15:16:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import List
-<<<<<<< HEAD
+
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from starlette.responses import FileResponse
@@ -7,7 +7,7 @@ from starlette.staticfiles import StaticFiles
 
 from db import Base, Entry, engine, Database
 from fastapi.middleware.cors import CORSMiddleware
-=======
+
 
 import uvicorn
 from fastapi import FastAPI, HTTPException, Depends
@@ -17,7 +17,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from pathlib import Path
->>>>>>> e51449a4cccf86c676c974ba641c75dab955a447
+
 
 app = FastAPI()
 database = Database()
@@ -30,19 +30,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-<<<<<<< HEAD
+
 app.mount("/frontend", StaticFiles(directory="frontend"), name="frontend")
 @app.get("/")
 async def read_root():
     return FileResponse('index.html')
-
-@app.post('/')
-=======
-app.mount("/", StaticFiles(directory=Path(__file__).parent.parent / "frontend"), name="frontend")
-
-@app.get("/")
-async def read_root():
-    return FileResponse(Path(__file__).parent.parent / "frontend" / "index.html")
 
 
 @app.on_event("shutdown")
@@ -50,7 +42,6 @@ async def shutdown_event():
     print("Shutting down gracefully...")
 
 @app.post('/api/submitForm')
->>>>>>> e51449a4cccf86c676c974ba641c75dab955a447
 async def receive_selected_numbers(submittedinfo: dict):
     selected_numbers = submittedinfo.get('selectedNumbers')
     input_text = submittedinfo.get('inputText')
@@ -59,10 +50,9 @@ async def receive_selected_numbers(submittedinfo: dict):
     message = f"Ausgewählte Zahlen erhalten: {selected_numbers}, Input-Wert: {input_text}, Pattern: {pattern}"
     print(message)
     database.save_submittedinfo(selected_numbers, input_text, pattern)
-<<<<<<< HEAD
+
     return {"message": message}
-=======
-    return {"message": message}
+
 
 if __name__ == "__main__":
     try:
@@ -70,4 +60,3 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("Server interrupted and shut down")
 
->>>>>>> e51449a4cccf86c676c974ba641c75dab955a447
